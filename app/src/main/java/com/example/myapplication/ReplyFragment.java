e51652b91e2247d1ef70d608d18877c9b9f3f43e
zhmeng <245944457@qq.com>
2020-06-18 02:08:09 +0800

    try push

--- app/src/main/java/com/example/myapplication/ReplyFragment.java
+++ app/src/main/java/com/example/myapplication/ReplyFragment.java
@@ -19,7 +19,6 @@ public class ReplyFragment extends Fragment {
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fg_content, container, false);
