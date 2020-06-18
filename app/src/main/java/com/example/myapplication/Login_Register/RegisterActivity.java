ba1476b57a8a2dda2729b5c039b6f49d069f0dea
刘海铭 <1252715377@qq.com>
2020-06-19 04:27:41 +0800

    普通用户注册写好了

--- app/src/main/java/com/example/myapplication/Login_Register/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/Login_Register/RegisterActivity.java
@@ -1,23 +1,29 @@
 package com.example.myapplication.Login_Register;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.MD5Utils;
 import com.example.myapplication.R;
+import com.example.myapplication.dao.UserDao;
+import com.example.myapplication.domain.User;
 
 
 public class RegisterActivity extends AppCompatActivity {
 
     //用户名，密码，再次输入的密码的控件
-    private EditText et_user_name, et_passWd, et_passsWd_again;
+    private EditText et_user_name, et_passWd, et_passsWd_again,et_department;
     private Button btn_register;
     private RadioGroup Sex;
+    private String user_sex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +41,7 @@ public class RegisterActivity extends AppCompatActivity {
         et_user_name = findViewById(R.id.et_user_name);
         et_passWd = findViewById(R.id.et_psw);
         et_passsWd_again= findViewById(R.id.et_psw_again);
+        et_department=findViewById(R.id.et_department);
         Sex= findViewById(R.id.SexRadio);
         //注册按钮
 //        btn_register.setOnClickListener(new View.OnClickListener() {
@@ -99,6 +106,47 @@ public class RegisterActivity extends AppCompatActivity {
     //注册按钮点击事件
     public void Register_reg(View v) {
 
+    }
+
+    public void Register(View view) {
+        UserDao userDao = new UserDao(this);
+        String test=et_user_name.getText().toString();
+
+        if (et_user_name.getText().toString().isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
+        } else if (et_passWd.getText().toString().isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+        } else if (et_passsWd_again.getText().toString().isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "请再次输入密码", Toast.LENGTH_SHORT).show();
+        } else if (user_sex.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "请选择性别", Toast.LENGTH_SHORT).show();
+        } else if (et_department.getText().toString().isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "请输入学院", Toast.LENGTH_SHORT).show();
+        } else if (!et_passWd.getText().toString().equals(et_passsWd_again.getText().toString())) {
+            Toast.makeText(RegisterActivity.this, "输入两次的密码不一样", Toast.LENGTH_SHORT).show();
+        } else if(userDao.findByUserName(et_user_name.getText().toString())!=null){
+            Toast.makeText(RegisterActivity.this, "该用户名已被注册", Toast.LENGTH_SHORT).show();
+        }else{
+            String password=et_passWd.getText().toString();
+            User user=new User(et_user_name.getText().toString(), MD5Utils.md5(et_passWd.getText().toString()),null,user_sex,et_department.getText().toString(),2);
+            userDao.add(user);
+            Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+            //注册成功后把账号传递到LoginActivity.java中
+            // 返回值到loginActivity显示
+            Intent data = new Intent();
+            data.putExtra("userName", user.getUserName());
+            setResult(RESULT_OK, data);
+            //RESULT_OK为Activity系统常量，状态码为-1，
+            // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
+            RegisterActivity.this.finish();
+        }
+    }
+
+    public void sureFemale(View view) {
+        user_sex="女";
+    }
+    public void sureMale(View view) {
+        user_sex="男";
     }
 }
 
