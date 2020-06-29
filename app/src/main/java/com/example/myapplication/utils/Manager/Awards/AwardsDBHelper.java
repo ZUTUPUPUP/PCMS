08b835407f3471b1576dfdc3ac8379073c361b8e
孙艺苹 <zut_syp@zut.edu.cn>
2020-06-29 21:47:58 +0800

    change DBHelper

--- app/src/main/java/com/example/myapplication/utils/Manager/Awards/AwardsDBHelper.java
+++ app/src/main/java/com/example/myapplication/utils/Manager/Awards/AwardsDBHelper.java
@@ -17,12 +17,12 @@ public class AwardsDBHelper extends SQLiteOpenHelper {
         db.execSQL("insert into awards values(null, '201708024105', '王二', '计科171', '蓝桥杯校赛', '冠军', '计算计学院')");
         db.execSQL("insert into awards values(null, '201708024106', '赵信', '计科171', '蓝桥杯校赛', '二等奖', '计算计学院')");
         db.execSQL("insert into awards values(null, '201708024107', '刘海', '计科172', '蓝桥杯校赛', '三等奖', '计算计学院')");
-        db.execSQL("insert into awards values(null, '201708024108', '刘晓', '计科172', 'acm选拔赛', '一等奖', '计算计学院')");
-        db.execSQL("insert into awards values(null, '201708024109', '张高', '计科173', 'acm选拔赛', '二等奖', '计算计学院')");
-        db.execSQL("insert into awards values(null, '201708024110', '孙吴', '计科174', 'acm选拔赛', '冠军', '理学院')");
-        db.execSQL("insert into awards values(null, '201708024111', '张楚', '计科175', 'acm校赛', '一等奖', '计算计学院')");
-        db.execSQL("insert into awards values(null, '201708024112', '张良', '计科176', 'acm校赛', '二等奖', '能源与环境学院')");
-        db.execSQL("insert into awards values(null, '201708024113', '刘辉', '计科177', 'acm校赛', '冠军', '计算计学院')");
+        db.execSQL("insert into awards values(null, '201708024108', '刘晓', '计科172', 'acm新生赛', '一等奖', '计算计学院')");
+        db.execSQL("insert into awards values(null, '201708024109', '张高', '计科173', 'acm新生赛', '二等奖', '计算计学院')");
+        db.execSQL("insert into awards values(null, '201708024110', '孙吴', '计科174', 'acm新生赛', '冠军', '理学院')");
+        db.execSQL("insert into awards values(null, '201708024111', '张楚', '计科175', 'acm新生赛', '一等奖', '计算计学院')");
+        db.execSQL("insert into awards values(null, '201708024112', '张良', '计科176', 'acm新生赛', '二等奖', '能源与环境学院')");
+        db.execSQL("insert into awards values(null, '201708024113', '刘辉', '计科177', 'acm新生赛', '冠军', '计算计学院')");
         db.execSQL("insert into awards values(null, '201708024114', '黄静', '计科178', 'acm校赛', '二等奖', '理学院')");
         db.execSQL("insert into awards values(null, '201708024115', '孙策贤', '计科179', 'acm校赛', '一等奖', '新闻与传播学院')");
         db.execSQL("insert into awards values(null, 'user', '用户', '计科179', 'acm校赛', '一等奖', '计算机学院')");
