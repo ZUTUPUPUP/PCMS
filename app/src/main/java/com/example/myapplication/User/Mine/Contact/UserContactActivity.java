bddfdaeb99806657c34800f4507597a0d2415e3e
RSHS <846844910@qq.com>
2020-08-15 18:33:25 +0800

    用户和管理员聊天，管理员和用户聊天，功能实现了，但写的很乱....

--- app/src/main/java/com/example/myapplication/User/Mine/Contact/UserContactActivity.java
+++ app/src/main/java/com/example/myapplication/User/Mine/Contact/UserContactActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -11,15 +12,23 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ScrollView;
 
+import com.alibaba.fastjson.JSON;
+import com.example.myapplication.Manager.Reply.ContactOneUserActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.dao.ContactDao;
 import com.example.myapplication.domain.Contact;
+import com.example.myapplication.utils.BaseUrl;
 import com.example.myapplication.utils.Contact.MassageItemAdapter;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+
+import okhttp3.Request;
 
 //用户界面下，用户联系管理员
 public class UserContactActivity extends AppCompatActivity {
@@ -32,6 +41,7 @@ public class UserContactActivity extends AppCompatActivity {
     private ContactDao contactDao;
     private MassageItemAdapter adapter;
     private List<Contact> contactList;
+    private List<Contact> adContactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +52,83 @@ public class UserContactActivity extends AppCompatActivity {
         getHistoryContact();//读取历史消息
         setBtnSendMes();//设置发送信息
     }
+    /**
+     * post请求
+     */
+    public void getDataGetByOKHttpUtils() {
+        String url = BaseUrl.BASE_URL + "contact/findAll.do";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .id(100)
+                .build()
+                .execute(new MMyStringCallback());
+    }
 
+    public class MMyStringCallback extends StringCallback {
+        @Override
+        public void onBefore(Request request, int id) {
+            //setTitle("loading...");
+        }
+
+        @Override
+        public void onAfter(int id) {
+            ///setTitle("Sample-okHttp");
+        }
+
+        @Override
+        public void onError(okhttp3.Call call, Exception e, int id) {
+            e.printStackTrace();
+//            tv_result.setText();
+//            Toast.makeText(RegisterActivity.this, "onError:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            // Log.v(TAG, "onError:" + e.getMessage());
+        }
+        @Override
+        public void onResponse(String response, int id) {
+            //  Log.e(TAG, "onResponse：complete");
+//            tv_result.setText();
+//            Toast.makeText(RegisterActivity.this, "onResponse:" + response, Toast.LENGTH_SHORT).show();
+            // Log.v(TAG, "onResponse:" + response);
+            switch (id) {
+                case 100:
+                    //  Toast.makeText(RegisterActivity.this, "http:数据请求成功",
+                    //          Toast.LENGTH_SHORT).show();
+                    break;
+                case 101:
+                    // Toast.makeText(RegisterActivity.this, "https:数据请求成功",
+                    //          Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            if(response != null) {
+                //解析数据
+                parseData(response);
+            }
+        }
+
+        @Override
+        public void inProgress(float progress, long total, int id) {
+            //Log.e(TAG, "inProgress:" + progress);
+        }
+    }
+
+    private void parseData(String json) {
+        contactList = JSON.parseArray(json, Contact.class);
+        adContactList = new ArrayList<>();
+        for(Contact contact:contactList){
+            if(contact.getSenderId().equals(userName)||contact.getReceiverId().equals(userName)){
+                adContactList.add(contact);
+            }
+        }
+        scrollView.post(new Runnable() {
+            public void run() {
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+        //筛选出admin和user的对话
+        adapter = new MassageItemAdapter(this,R.layout.item_contactmessage, adContactList);
+        showListView.setAdapter(adapter);
+        showListView.setSelection(adapter.getCount()-1);
+    }
     public  void init(){
         Intent getIntent = getIntent();
         userName = getIntent.getStringExtra("userName");  //System.out.println(userName);
@@ -57,11 +143,7 @@ public class UserContactActivity extends AppCompatActivity {
                 scrollView.fullScroll(View.FOCUS_DOWN);
             }
         });
-        contactList=contactDao.findById(userName);
-        //筛选出admin和user的对话
-        adapter = new MassageItemAdapter(this,R.layout.item_contactmessage, contactList);
-        showListView.setAdapter(adapter);
-        showListView.setSelection(adapter.getCount()-1);
+       getDataGetByOKHttpUtils();
 
     }
     private void setBtnSendMes() {
@@ -75,9 +157,9 @@ public class UserContactActivity extends AppCompatActivity {
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm");
                 String nowDate=(sdf.format(calendar.getTime()));
                 contact.setTimestamp(nowDate);
-                contactDao.insertOneContact(contact);
+                postOKHttpUtils(contact);
                 //插入一个聊天记录后要刷新ListVeiw
-                contactList.add(contact);
+                adContactList.add(contact);
                 adapter.notifyDataSetChanged();//关键
                 //强制到底
                 scrollView.post(new Runnable() {
@@ -92,7 +174,64 @@ public class UserContactActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * post请求
+     */
+    public void postOKHttpUtils(Contact contact) {
+        String url = BaseUrl.BASE_URL + "contact/insertOne.do";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .id(1001)
+                .addParams("timestamp",contact.getTimestamp())
+                .addParams("senderId",contact.getSenderId())
+                .addParams("receiverId",contact.getReceiverId())
+                .addParams("mas",contact.getMas())
+                .build()
+                .execute(new PostStringCallback());
+    }
 
+    public class  PostStringCallback extends StringCallback {
+        @Override
+        public void onBefore(Request request, int id) {
+            //setTitle("loading...");
+        }
+
+        @Override
+        public void onAfter(int id) {
+            ///setTitle("Sample-okHttp");
+        }
+
+        @Override
+        public void onError(okhttp3.Call call, Exception e, int id) {
+            e.printStackTrace();
+//            tv_result.setText();
+//            Toast.makeText(RegisterActivity.this, "onError:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            // Log.v(TAG, "onError:" + e.getMessage());
+        }
+        @Override
+        public void onResponse(String response, int id) {
+            Log.e("ContactOneUserUserAct", "onResponse：complete");
+//            tv_result.setText();
+//            Toast.makeText(RegisterActivity.this, "onResponse:" + response, Toast.LENGTH_SHORT).show();
+            // Log.v(TAG, "onResponse:" + response);
+            switch (id) {
+                case 100:
+                    //  Toast.makeText(RegisterActivity.this, "http:数据请求成功",
+                    //          Toast.LENGTH_SHORT).show();
+                    break;
+                case 101:
+                    // Toast.makeText(RegisterActivity.this, "https:数据请求成功",
+                    //          Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
+        @Override
+        public void inProgress(float progress, long total, int id) {
+            //Log.e(TAG, "inProgress:" + progress);
+        }
+    }
 
 
     String UTCtoUTF8(String dateStr) { //utc转到utf8
--- app/src/main/java/com/example/myapplication/domain/Contact.java
+++ app/src/main/java/com/example/myapplication/domain/Contact.java
@@ -3,13 +3,13 @@ package com.example.myapplication.domain;
 *一条聊天记录
  */
 public class Contact {
-    private int _id; //聊天记录的id
+    private Integer _id; //聊天记录的id
     private String timestamp; //时间戳
     private String senderId;//发送者
     private String receiverId;//接收者
     private String mas; //消息内容
 
-    public Contact(int _id, String timestamp, String senderId, String receiverId, String mas) {
+    public Contact(Integer _id, String timestamp, String senderId, String receiverId, String mas) {
         this._id = _id;
         this.timestamp = timestamp;
         this.senderId = senderId;
@@ -17,44 +17,8 @@ public class Contact {
         this.mas = mas;
     }
 
-    public int get_id() {
-        return _id;
-    }
+    public Contact() {
 
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public String getSenderId() {
-        return senderId;
-    }
-
-    public String getReceiverId() {
-        return receiverId;
-    }
-
-    public String getMas() {
-        return mas;
-    }
-
-    public void set_id(int _id) {
-        this._id = _id;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public void setSenderId(String senderId) {
-        this.senderId = senderId;
-    }
-
-    public void setReceiverId(String receiverId) {
-        this.receiverId = receiverId;
-    }
-
-    public void setMas(String mas) {
-        this.mas = mas;
     }
 
     @Override
@@ -66,5 +30,45 @@ public class Contact {
                 ", receiverId='" + receiverId + '\'' +
                 ", mas='" + mas + '\'' +
                 '}';
+    }
+
+    public Integer get_id() {
+        return _id;
+    }
+
+    public void set_id(Integer _id) {
+        this._id = _id;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getMas() {
+        return mas;
+    }
+
+    public void setMas(String mas) {
+        this.mas = mas;
     }
 }
