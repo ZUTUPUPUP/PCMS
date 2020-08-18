e4b7473592f5de098eb37d40578bff8a81dc3dab
刘海铭 <1252715377@qq.com>
2020-08-18 20:35:53 +0800

    服务器地址

--- app/src/main/java/com/example/myapplication/utils/BaseUrl.java
+++ app/src/main/java/com/example/myapplication/utils/BaseUrl.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.utils;
 
 public class BaseUrl {
-    public static final String BASE_URL = "http://192.168.42.250:8080/";
+    //阿里云服务器
+    public static final String BASE_URL = "http://47.113.81.51:8080/ssm/";
 }
