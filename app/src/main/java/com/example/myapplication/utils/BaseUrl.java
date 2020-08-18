d95242f36af5f9e208b0d413d4e9588124bdbebb
RSHS <846844910@qq.com>
2020-08-18 10:04:40 +0800

    注册界面ui修改

--- app/src/main/java/com/example/myapplication/utils/BaseUrl.java
+++ app/src/main/java/com/example/myapplication/utils/BaseUrl.java
@@ -1,5 +1,5 @@
 package com.example.myapplication.utils;
 
 public class BaseUrl {
-    public static final String BASE_URL = "http://192.168.0.108:8080/Login/";
+    public static final String BASE_URL = "http://10.30.34.89:8080/ProgrammingContest_war_exploded/";
 }
